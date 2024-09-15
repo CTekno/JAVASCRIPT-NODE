a29d3855883c39cdcb4026fec0ecb7ff5ec80c1c
CaioT <caio.m.taparo@gmail.com>
2024-09-15 12:58:17 -0300

    sqlite

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
-const Database = require('better-sqlite3');
+const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3');
 const path = require('path');
 
 // Definindo o caminho do arquivo do banco de dados SQLite
@@ -6,36 +7,41 @@ const dbPath = path.resolve(__dirname, 'mitproject.db');
 
 // Conectando ao banco de dados SQLite
 let connection;
-try {
-    connection = new Database(dbPath, { verbose: console.log });
-    console.log('Conectado ao banco de dados SQLite.');
+(async () => {
+    try {
+        connection = await sqlite.open({
+            filename: dbPath,
+            driver: sqlite3.Database
+        });
+        console.log('Conectado ao banco de dados SQLite.');
 
-    // Deletando e recriando as tabelas
-    connection.exec(`
-        DROP TABLE IF EXISTS usuarios;
-        DROP TABLE IF EXISTS palavras;
-        
-        CREATE TABLE usuarios (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            login TEXT,
-            email TEXT NOT NULL,
-            senha TEXT NOT NULL
-        );
+        // Deletando e recriando as tabelas
+        await connection.exec(`
+            DROP TABLE IF EXISTS usuarios;
+            DROP TABLE IF EXISTS palavras;
 
-        CREATE TABLE palavras (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            rank INTEGER,
-            nome TEXT NOT NULL,
-            descricao TEXT NOT NULL,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-        );
-    `);
-    
-    console.log('Tabelas recriadas com sucesso.');
-    
-} catch (err) {
-    console.error('Erro ao conectar ao SQLite:', err.message);
-    throw err;
-}
+            CREATE TABLE usuarios (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                login TEXT,
+                email TEXT NOT NULL,
+                senha TEXT NOT NULL
+            );
+
+            CREATE TABLE palavras (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                rank INTEGER,
+                nome TEXT NOT NULL,
+                descricao TEXT NOT NULL,
+                created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+
+        console.log('Tabelas recriadas com sucesso.');
+
+    } catch (err) {
+        console.error('Erro ao conectar ao SQLite:', err.message);
+        throw err;
+    }
+})();
 
 module.exports = connection;
